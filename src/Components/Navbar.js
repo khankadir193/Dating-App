bc688582b64443a2a0e1f97692789b06e3181f91
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-06-27 19:26:19 +0530

    dating-app

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -7,10 +7,12 @@ import AccountCircle from '@mui/icons-material/AccountCircle';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import ProfileImage from './ProfileImage';
+import Account from './Account';
 
 const Navbar = ({user, onLogout}) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const {name,email,picture} = user;
+  const [account,setAccount] = useState();
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
@@ -20,7 +22,13 @@ const Navbar = ({user, onLogout}) => {
     setAnchorEl(null);
   };
 
+  const hanldeProfile = ()=>{
+    setAccount(true);
+    setAnchorEl(null);
+  }
+
   return (
+    <>
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
@@ -56,12 +64,14 @@ const Navbar = ({user, onLogout}) => {
             onClose={handleClose}
           >
             <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem onClick={handleClose}>My account</MenuItem>
+            <MenuItem onClick={hanldeProfile}>My account</MenuItem>
             <MenuItem onClick={onLogout}>Logout</MenuItem>
           </Menu>
         </div>
       </Toolbar>
     </AppBar>
+    {account && <Account />}
+    </>
   );
 };
 
