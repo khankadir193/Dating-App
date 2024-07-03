08a53f5b0ee93ae6b669605a8343bec4c6ce59df
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-03 15:30:15 +0530

    SwipeableComp has been comented

--- src/Components/SwipeableComp.js
+++ src/Components/SwipeableComp.js
@@ -1,43 +1,43 @@
-// src/components/SwipeableComponent.js
-import React from 'react';
-import SwipeableViews from 'react-swipeable-views';
-import { makeStyles } from '@mui/styles';
+// // src/components/SwipeableComponent.js
+// import React from 'react';
+// import SwipeableViews from 'react-swipeable-views';
+// import { makeStyles } from '@mui/styles';
 
-const useStyles = makeStyles({
-  slide: {
-    padding: 15,
-    minHeight: 100,
-    color: '#fff',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontSize: '24px',
-  },
-  slide1: {
-    background: '#FEA900',
-  },
-  slide2: {
-    background: '#B3DC4A',
-  },
-  slide3: {
-    background: '#6AC0FF',
-  },
-});
+// const useStyles = makeStyles({
+//   slide: {
+//     padding: 15,
+//     minHeight: 100,
+//     color: '#fff',
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     fontSize: '24px',
+//   },
+//   slide1: {
+//     background: '#FEA900',
+//   },
+//   slide2: {
+//     background: '#B3DC4A',
+//   },
+//   slide3: {
+//     background: '#6AC0FF',
+//   },
+// });
 
-const SwipeableComp = () => {
-  const classes = useStyles();
+// const SwipeableComp = () => {
+//   const classes = useStyles();
 
-  const handleChangeIndex = (index) => {
-    console.log('Current index is', index);
-  };
+//   const handleChangeIndex = (index) => {
+//     console.log('Current index is', index);
+//   };
 
-  return (
-    <SwipeableViews onChangeIndex={handleChangeIndex} resistance animateHeight axis='y'>
-      <div className={`${classes.slide} ${classes.slide1}`}>Slide 1</div>
-      <div className={`${classes.slide} ${classes.slide2}`}>Slide 2</div>
-      <div className={`${classes.slide} ${classes.slide3}`}>Slide 3</div>
-    </SwipeableViews>
-  );
-};
+//   return (
+//     <SwipeableViews onChangeIndex={handleChangeIndex} resistance animateHeight axis='y'>
+//       <div className={`${classes.slide} ${classes.slide1}`}>Slide 1</div>
+//       <div className={`${classes.slide} ${classes.slide2}`}>Slide 2</div>
+//       <div className={`${classes.slide} ${classes.slide3}`}>Slide 3</div>
+//     </SwipeableViews>
+//   );
+// };
 
-export default SwipeableComp;
+// export default SwipeableComp;
