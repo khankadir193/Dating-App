6fd04c60730c1714e9292121cc2cf1b6b0552dc0
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-08 17:53:41 +0530

    user component has been refactored

--- src/Components/UserComponent.js
+++ src/Components/UserComponent.js
@@ -1,35 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import backendIntegration from './BackendIntegration'; // Import the utility function
+import backendIntegration from './BackendIntegration'; // Import the utility module
 
 const UserComponent = ({ userData }) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      //inserting data
-      // try {
-      //   const result = await backendIntegration.postData(userData);
-      //   setData(result); // Set the received data to state
-      // } catch (err) {
-      //   setError(err.message); // Set error message to state
-      // } finally {
-      //   setLoading(false); // Set loading state to false
-      // }
+  // Function to insert data
+  const storeData = async () => {
+    try {
+      const result = await backendIntegration.postData(userData);
+      setData(result); // Set the received data to state
+    } catch (err) {
+      setError(err.message); // Set error message to state
+    } finally {
+      setLoading(false); // Set loading state to false
+    }
+  };
 
-      // getting data
-      try {
-        const result = await backendIntegration.getData();
-        setData(result); // Set the received data to state
-      } catch (err) {
-        setError(err.message); // Set error message to state
-      } finally {
-        setLoading(false); // Set loading state to false
-      }
+  // Function to fetch data
+  const fetchData = async () => {
+    try {
+      const result = await backendIntegration.getData();
+      setData(result); // Set the received data to state
+      return result;
+    } catch (err) {
+      setError(err.message); // Set error message to state
+    } finally {
+      setLoading(false); // Set loading state to false
+    }
+  };
+
+  useEffect(() => {
+    const performDataOperations = async () => {
+      // await storeData();
+      const res = await fetchData();
+      console.log('fetchData function', res);
     };
 
-    fetchData();
+    performDataOperations();
   }, [userData]);
 
   if (loading) {
