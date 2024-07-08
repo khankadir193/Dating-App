71ee1281bc2170e32ca9988eaf881571f59af064
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-08 16:49:08 +0530

    backend integration get && post method call

--- src/Components/BackendIntegration.js
+++ src/Components/BackendIntegration.js
@@ -1,29 +1,48 @@
-// backendIntegration.js
+const backendIntegration = {
+  postData: async (userData) => {
+    const url = 'http://localhost:3001/insert';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userData)
+    };
 
-const backendIntegration = async (userData) => {
-  const url = 'http://localhost:3001/insert';
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(userData)
-  };
+    try {
+      const response = await fetch(url, options);
 
-  try {
-    const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
+      }
 
-    if (!response.ok) {
-      throw new Error(`Network response was not ok: ${response.statusText}`);
+      const data = await response.json();
+      console.log('Response data:', data);
+      return data;  // Return the data for further use if needed
+    } catch (error) {
+      console.error('Error during fetch operation:', error);
+      throw error;  // Re-throw the error to allow handling in the calling code
     }
+  },
 
-    const data = await response.json();
-    console.log('Response data:', data);
-    return data;  // Return the data for further use if needed
-  } catch (error) {
-    console.error('Error during fetch operation:', error);
-    throw error;  // Re-throw the error to allow handling in the calling code
-  }
+  // getData: async () => {
+  //   const url = 'http://localhost:3001/gettingData';
+
+  //   try {
+  //     const response = await fetch(url);
+
+  //     if (!response.ok) {
+  //       throw new Error(`Network response was not ok: ${response.statusText}`);
+  //     }
+
+  //     const data = await response.json();
+  //     console.log('getting Response data:', data);
+  //     return data;  // Return the data for further use if needed
+  //   } catch (error) {
+  //     console.error('Error during fetch operation:', error);
+  //     throw error;  // Re-throw the error to allow handling in the calling code
+  //   }
+  // }
 };
 
 export default backendIntegration;
